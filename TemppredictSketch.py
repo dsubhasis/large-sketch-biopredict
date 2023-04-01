002200cb789fcd28264f78c9d4617557f50dd8af
Subhasis Dasgupta <sudasgupta@sdsc.edu>
2023-04-01 23:04:59 +0530

    Minor solve

--- TemppredictSketch.py
+++ TemppredictSketch.py
@@ -83,7 +83,7 @@ def split_dictionary(input_dict, chunk_size):
             new_dict = {k: v}
     res.append(new_dict)
     return res
-dicts = mydict
+dicts = split_dictionary(mydict, 1)
 
 def uprocess(mydict):
     for key, value in mydict.items():
